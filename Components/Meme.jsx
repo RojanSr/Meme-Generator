659789c32d029e8ccf692a8f2164b696ddc78252
RojanSr <rairojan1116@gmail.com>
2023-05-13 16:57:55 +0545

    button click logs random image url

--- Components/Meme.jsx
+++ Components/Meme.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import memeImg from "../memesData";
 
 export default function Meme() {
+  function generateMeme() {
+    if (memeImg.success === true) {
+      const randIndex = Math.floor(Math.random() * memeImg.data.memes.length);
+
+      let url = memeImg.data.memes[randIndex].url;
+
+      console.log(url);
+      return url;
+    }
+  }
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" className="form--input" placeholder="Top text" />
         <input type="text" className="form--input" placeholder="Bottom text" />
-        <button className="form--button">Get a new Image 🌄</button>
-      </form>
+        <button onClick={generateMeme} className="form--button">
+          Get a new Image 🌄
+        </button>
+      </div>
     </main>
   );
 }
